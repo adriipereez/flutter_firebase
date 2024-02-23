4578aa1fb225af1c1b8bc9d877180d7f2f775ba5
adriipereez09 <adriaperez@ceroca.cat>
2024-02-23 19:38:44 +0100

    UI Login Safe Area y SingleChildScrollView

--- lib/paginas/pagina_login.dart
+++ lib/paginas/pagina_login.dart
@@ -18,84 +18,89 @@ class _PaginaloginState extends State<Paginalogin> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 250, 183, 159),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-        
-            //LOGO
-            const Icon(
-             Icons.fireplace,
-             size: 120,
-             color: Color.fromARGB(255, 255, 240, 218 ), 
-            ),
-            const SizedBox(height: 10,),
-            //FRASE
-            const Text(
-              "Benvingut/da de nou",
-              style: TextStyle(
-                color: Color.fromARGB(255, 255, 240, 218),
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            const SizedBox(height: 25,),
-            //Text divisori
-            const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 25),
-              child: Row(
+      body: SafeArea(
+        child: Center(
+          child: SingleChildScrollView(
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Expanded(
-                    child: Divider(
-                      thickness: 1,
-                      color: Color.fromARGB(255, 255, 240, 218),
-                    ),
+                  //LOGO
+                  const Icon(
+                   Icons.fireplace,
+                   size: 120,
+                   color: Color.fromARGB(255, 255, 240, 218 ), 
                   ),
-                  Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 4),
-                    child: Text(
-                      "LOGUEATE",
-                      style: TextStyle(color: Color.fromARGB(255, 255, 250, 218),),
-                    ),
-                  ),
-                  Expanded(
-                    child: Divider(
-                      thickness: 1,
-                      color: Color.fromARGB(255, 255, 240, 218),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            const SizedBox(height: 10,),
-            //TextField Email
-            TextFieldAuth(controller: controllerEmail, obscureText: false, hintText: "Email"),
-            //TextField Password
-            TextFieldAuth(controller: controllerContrasena, obscureText: true, hintText: "Password"),
-            const SizedBox(height: 10,),
-            //No estas registrado
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 25),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Text("¿No eres miembro?"),
-                  SizedBox(width: 5,),
-                  GestureDetector(
-                    onTap: (){},
-                    child: const Text("Registarte",
+                  const SizedBox(height: 10,),
+                  //FRASE
+                  const Text(
+                    "Benvingut/da de nou",
                     style: TextStyle(
+                      color: Color.fromARGB(255, 255, 240, 218),
+                      fontSize: 18,
                       fontWeight: FontWeight.bold,
-                      color: Color.fromARGB(255, 44, 174, 255),
-                    ),),
+                    ),
                   ),
+                  const SizedBox(height: 25,),
+                  //Text divisori
+                  const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 25),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: Divider(
+                            thickness: 1,
+                            color: Color.fromARGB(255, 255, 240, 218),
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 4),
+                          child: Text(
+                            "LOGUEATE",
+                            style: TextStyle(color: Color.fromARGB(255, 255, 250, 218),),
+                          ),
+                        ),
+                        Expanded(
+                          child: Divider(
+                            thickness: 1,
+                            color: Color.fromARGB(255, 255, 240, 218),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 10,),
+                  //TextField Email
+                  TextFieldAuth(controller: controllerEmail, obscureText: false, hintText: "Email"),
+                  //TextField Password
+                  TextFieldAuth(controller: controllerContrasena, obscureText: true, hintText: "Password"),
+                  const SizedBox(height: 10,),
+                  //No estas registrado
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 25),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Text("¿No eres miembro?"),
+                        SizedBox(width: 5,),
+                        GestureDetector(
+                          onTap: (){},
+                          child: const Text("Registarte",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Color.fromARGB(255, 44, 174, 255),
+                          ),),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 10,),
+                  //Boton login
+                  const BotonAuth(),
                 ],
               ),
             ),
-            const SizedBox(height: 10,),
-            //Boton login
-            const BotonAuth(),
-          ],
+          ),
         ),
       ),
     );
