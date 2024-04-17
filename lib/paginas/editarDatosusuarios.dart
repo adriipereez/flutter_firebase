15861cae6a62b0e1a48463a81c746487d7eb8c7b
adriipereez09 <adriaperez@ceroca.cat>
2024-04-17 19:17:42 +0200

    Ejemplo completo

--- lib/paginas/editarDatosusuarios.dart
+++ lib/paginas/editarDatosusuarios.dart
@@ -4,6 +4,8 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter_firebase/auth/servicio_auth.dart';
 
 class EditarDatosUsuario extends StatefulWidget {
   const EditarDatosUsuario({super.key});
@@ -33,20 +35,72 @@ class _EditarDatosUsuarioState extends State<EditarDatosUsuario> {
         });
       }
 
-     if(kIsWeb){
+      if (kIsWeb) {
         Uint8List archivoEnBytes = await image.readAsBytes();
 
         setState(() {
-          
           _imagenSeleccionadaWeb = archivoEnBytes;
           _imagenAPuntoParaSubir = true;
         });
-     }
+      }
+    }
+  }
 
+  Future<bool> pujarImatgePerUsuari() async {
+    String idUsuari = ServicioAuth().getUsuarioActual()!.uid;
 
+    Reference ref =
+        FirebaseStorage.instance.ref().child("$idUsuari/avatar/$idUsuari");
+
+    // Agafem la imatge de la variable que la tingui (la de web o la de App).
+    if (_imagenSeleccionadaApp != null) {
+      try {
+        await ref.putFile(_imagenSeleccionadaApp!);
+        return true;
+      } catch (e) {
+        return false;
+      }
     }
 
-      
+    if (_imagenSeleccionadaWeb != null) {
+      try {
+        await ref.putData(_imagenSeleccionadaWeb!);
+        return true;
+      } catch (e) {
+        return false;
+      }
+    }
+
+    return false;
+  }
+
+  Future<String> getImatgePerfil() async {
+    final String idUsuari = ServicioAuth().getUsuarioActual()!.uid;
+    final Reference ref =
+        FirebaseStorage.instance.ref().child("$idUsuari/avatar/$idUsuari");
+
+    final String urlImatge = await ref.getDownloadURL();
+
+    return urlImatge;
+  }
+
+  Widget mostrarImatge() {
+    return FutureBuilder(
+      future: getImatgePerfil(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting ||
+            snapshot.hasError) {
+          return const Icon(Icons.person);
+        }
+
+        return Image.network(
+          snapshot.data!,
+          errorBuilder: (context, error, stackTrace) {
+            return Text("Error al carregar la imatge: $error");
+          },
+        );
+      },
+    );
   }
 
   @override
@@ -73,15 +127,43 @@ class _EditarDatosUsuarioState extends State<EditarDatosUsuario> {
             // Boton para subir la imagen seleccionada con FilePicker
             // ======================================================
             GestureDetector(
-              onTap: () {},
+              onTap: () async {
+                if (_imagenAPuntoParaSubir) {
+                  bool imatgePujadaCorrectament = await pujarImatgePerUsuari();
+
+                  if (imatgePujadaCorrectament) {
+                    mostrarImatge();
+                    setState(() {});
+                  }
+                }
+              },
               child: Container(
                 padding: const EdgeInsets.all(10),
                 decoration: BoxDecoration(color: Colors.purpleAccent),
                 child: const Text("Sube una imagen"),
               ),
-            )
+            ),
+            // Visor del resultat del FilePicker.
+            // ==================================
+            Container(
+              child: _imagenSeleccionadaWeb == null &&
+                      _imagenSeleccionadaApp == null
+                  ? Container()
+                  : kIsWeb
+                      ? Image.memory(
+                          _imagenSeleccionadaWeb!,
+                          fit: BoxFit.fill,
+                        )
+                      : Image.file(
+                          _imagenSeleccionadaApp!,
+                          fit: BoxFit.fill,
+                        ),
+            ),
             // Visor del resultado de cargar la imagen de Firebase Storage
             // ===========================================================
+            Container(
+              child: mostrarImatge(),
+            ),
           ],
         ),
       )),
